0ead2d659bc561d9020d0ad21cd92509a7955807
Joao Victor Moraes Vieira <88463022+jaummm1@users.noreply.github.com>
2025-10-08 21:42:29 -0300

    Add files via upload

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -10,9 +10,10 @@ const app = express()
 import cors from 'cors'
 
 app.use(cors({
-    origin: process.env.ALLOWED_ORIGINS.split(','),
-    // credentials: true
+  origin: process.env.ALLOWED_ORIGINS.split(','),
+  // credentials: true
 }))
+
 app.use(logger('dev'))
 app.use(json())
 app.use(urlencoded({ extended: false }))
